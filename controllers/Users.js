ee342918b86d5298377de8c40f41d678462d7741
SAHIL HOSSAIN <111786720+Sahil1786@users.noreply.github.com>
2025-08-25 16:56:40 +0530

    Update forget-password

--- controllers/Users.js
+++ controllers/Users.js
@@ -244,7 +244,52 @@ router.get("/check-auth", authMiddleware, async (req, res) => {
 
 
 
-router.post("/forgot-password", async (req, res) => {
+// router.post("/forgot-password", async (req, res) => {
+//   try {
+//     const { login_id, new_password } = req.body;
+
+//     if (!login_id || !new_password) {
+//       return res.status(400).json({
+//         message: "Login ID and new password are required"
+//       });
+//     }
+
+
+//     const [results] = await connection.execute(
+//       "SELECT * FROM api_dashboard_user WHERE login_id = ?",
+//       [login_id]
+//     );
+
+//     if (results.length === 0) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+
+
+//     const hashedPassword = md5(new_password);
+
+  
+//     const [updateResult] = await connection.execute(
+//       "UPDATE api_dashboard_user SET password = ?, update_on = NOW() WHERE login_id = ?",
+//       [hashedPassword, login_id]
+//     );
+
+//     if (updateResult.affectedRows === 0) {
+//       return res.status(500).json({ message: "Password reset failed" });
+//     }
+
+//     return res.status(200).json({
+//       message: "Password reset successful"
+//     });
+
+//   } catch (err) {
+//     console.error("Forgot Password error:", err);
+//     return res.status(500).json({ message: "Internal server error" });
+//   }
+// });
+
+
+
+router.put("/forgot-password", authMiddleware, async (req, res) => {
   try {
     const { login_id, new_password } = req.body;
 
@@ -254,9 +299,9 @@ router.post("/forgot-password", async (req, res) => {
       });
     }
 
-
+  
     const [results] = await connection.execute(
-      "SELECT * FROM api_dashboard_user WHERE login_id = ?",
+      "SELECT id FROM api_dashboard_user WHERE login_id = ?",
       [login_id]
     );
 
@@ -267,9 +312,9 @@ router.post("/forgot-password", async (req, res) => {
 
     const hashedPassword = md5(new_password);
 
-  
+   
     const [updateResult] = await connection.execute(
-      "UPDATE api_dashboard_user SET password = ?, update_on = NOW() WHERE login_id = ?",
+      "UPDATE api_dashboard_user SET password = ?, update_on = DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s') WHERE login_id = ?",
       [hashedPassword, login_id]
     );
 
@@ -286,6 +331,7 @@ router.post("/forgot-password", async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 });
+
 
 
 
